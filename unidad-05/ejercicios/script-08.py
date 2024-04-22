f141b60f47eec17eb575beb1e012cc180a246d65
Franco Sbaffi <francosbaffidev@gmail.com>
2024-04-22 06:50:54 -0300

    Unidad 5 Finalizada

--- unidad-05/ejercicios/script-08.py
+++ unidad-05/ejercicios/script-08.py
@@ -5,26 +5,4 @@
 # Utiliza un bucle for junto con índices para recorrer la mitad del string y comparar los caracteres desde el principio con los caracteres desde el final.
 # Si todos los caracteres coinciden, indica que la palabra es un palíndromo; de lo contrario, indica lo contrario.
 
-# Solicitar al usuario que ingrese una palabra
-palabra = input("Ingresa una palabra: ")
 
-# Convertir la palabra a minúsculas
-palabra = palabra.lower()
-
-# Inicializar una bandera para indicar si la palabra es un palíndromo
-es_palindromo = True
-
-# Utilizar un bucle for junto con índices para recorrer la mitad del string
-for i in range(len(palabra) // 2):
-    # Comparar los caracteres desde el principio con los caracteres desde el final
-    if palabra[i] != palabra[-(i + 1)]:
-        # Si hay al menos una diferencia, la palabra no es un palíndromo
-        es_palindromo = False
-        # Salir del bucle porque ya no es necesario seguir verificando
-        break
-
-# Verificar si la palabra es un palíndromo
-if es_palindromo:
-    print("La palabra es un palíndromo.")
-else:
-    print("La palabra no es un palíndromo.")
